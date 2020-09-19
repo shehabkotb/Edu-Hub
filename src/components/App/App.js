7cb18187042c371bd04fccc1c6bb36a70c9efc60
Eslam Genedy <eslamgenedy377@yahoo.com>
2020-09-19 18:07:47 +0200

    adding Dashboards ,Login, SignUp

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = ()=> {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Learn React
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/StudentDashboard/StudentDashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StudentDashboard = ()=>{
+    return(
+        <div>StudentDashboard</div>
+    )
+}
+
+export default StudentDashboard ; 
