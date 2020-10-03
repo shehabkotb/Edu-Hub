f5be37e14ff7eb80f4db17ad88f50466cae7e800
Eslam Genedy <eslamgenedy377@yahoo.com>
2020-10-04 00:49:47 +0200

    add: form validation for login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,34 +1,99 @@
 import React, { Component } from 'react';
 import './Login.css'
 
+const validEmailRegex = RegExp(
+    /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i    
+    );
+const validateForm = errors => {
+    let valid = true;
+    Object.values(errors).forEach(val => val.length > 0 && (valid = false));
+    return valid;
+};
+
 
 class Login extends Component {
     constructor(props) {
         super(props);
-        this.state = { value: 'coconut' };
+        this.state = {
+           
+            email: null,
+            password: null,
+            errors: {
+                
+                email: '',
+                password: '',
+            }
+        };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) { this.setState({ value: event.target.value }); }
-    handleSubmit(event) {
-        alert('Your favorite flavor is: ' + this.state.value);
+    handleChange = (event) => {
         event.preventDefault();
+        const { name, value } = event.target;
+        let errors = this.state.errors;
+
+        switch (name) {
+            case 'email':
+                errors.email =
+                    validEmailRegex.test(value)
+                        ? ''
+                        : 'Email is not valid!';
+                break;
+            case 'password':
+                errors.password =
+                    value.length < 8
+                        ? 'Password must be at least 8 characters long!'
+                        : '';
+                break;
+            
+            default:
+                break;
+        }
+
+        this.setState({ errors, [name]: value })
     }
 
+    handleSubmit = (event) => {
+        event.preventDefault();
+        if (validateForm(this.state.errors)) {
+            console.info('Valid Form')
+        } else {
+            console.error('Invalid Form')
+        }
+    }
     render() {
+        const { errors } = this.state;
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={this.handleSubmit} noValidate>
                 <div class='label'>Enjoy Learning With Us</div>
                 <div class='div-form'>
-                    <label>Student_Id <span>*</span> </label>
-                    <input type="text" name="Student_Id" placeholder="Student Id" required />
+                  
+
+                    <label>Email  <span>*</span></label>
+                    <input 
+                        type="email" 
+                        name="Email" 
+                        placeholder="email" 
+                        noValidate 
+                        onChange={this.handleChange}
+                    />
+                     {errors.email.length > 0 &&
+                        (<label className='err'>{errors.email}</label>)
+                    }
 
                     <label>Password  <span>*</span></label>
-                    <input type="password" name="Password" placeholder="Password" required />
-
-                    <div class="g-recaptcha" data-sitekey="6Ldbdg0TAAAAAI7KAf72Q6uagbWzWecTeBWmrCpJ">
-                    </div>
+                    <input
+                     type="password" 
+                     name="password" 
+                     placeholder="Password" 
+                     noValidate
+                     onChange={this.handleChange}
+                    />
+                    {errors.password.length > 0 &&
+                        (<label className='err'>{errors.password}</label>)
+                    }
+                    
                     <input type="submit" value="Join Now" class='submit' />
                 </div>
             </form>
--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom'
-import { createStore } from "redux";
-import { Provider } from "react-redux";
+// import { createStore } from "redux";
+// import { Provider } from "react-redux";
 // import reducer from "./reducers";
-import middleware from "./middlewares";
+// import middleware from "./middlewares";
 
 
-const store = createStore(reducer, middleware);
+// const store = createStore(reducer, middleware);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
