6c0ec758219b36c3730a9964d5c8b8028daf536b
Eslam Genedy <eslamgenedy377@yahoo.com>
2020-10-03 16:39:34 +0200

    fix: updating the style of Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react';
+import './Login.css'
 
-const Login = ()=>{
-    return(
-        <div>Login</div>
-    );
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: 'coconut' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) { this.setState({ value: event.target.value }); }
+    handleSubmit(event) {
+        alert('Your favorite flavor is: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div class='label'>Enjoy Learning With Us</div>
+                <div class='div-form'>
+                    <label>Student_Id <span>*</span> </label>
+                    <input type="text" name="Student_Id" placeholder="Student Id" required />
+
+                    <label>Password  <span>*</span></label>
+                    <input type="password" name="Password" placeholder="Password" required />
+
+                    <div class="g-recaptcha" data-sitekey="6Ldbdg0TAAAAAI7KAf72Q6uagbWzWecTeBWmrCpJ">
+                    </div>
+                    <input type="submit" value="Join Now" class='submit' />
+                </div>
+            </form>
+        );
+    }
 }
 
-export default Login ; 
+export default Login; 
