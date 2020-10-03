72d632257bb5fd2fb011d51b5cbb41660d8383bd
Eslam Genedy <eslamgenedy377@yahoo.com>
2020-10-03 16:34:11 +0200

    fix: updating the style of SignUp

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,34 +1,46 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
+import './SignUp.css'
+
 
 class SignUp extends Component {
     constructor(props) {
-      super(props);
-      this.state = {value: 'coconut'};
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+        super(props);
+        this.state = { value: 'coconut' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-  
-    handleChange(event) {    this.setState({value: event.target.value});  }
-    handleSubmit(event) {
-      alert('Your favorite flavor is: ' + this.state.value);
-      event.preventDefault();
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Pick your favorite flavor:
-            <select value={this.state.value} onChange={this.handleChange}>            <option value="grapefruit">Grapefruit</option>
-              <option value="lime">Lime</option>
-              <option value="coconut">Coconut</option>
-              <option value="mango">Mango</option>
-            </select>
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      );
-    }
-  }
 
-  export default SignUp; 
+    handleChange(event) { this.setState({ value: event.target.value }); }
+    handleSubmit(event) {
+        alert('Your favorite flavor is: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div class='label'>Enjoy Learning With Us</div>
+                <div class='div-form'>
+                    <label>Student_Id <span>*</span> </label>
+                    <input type="text" name="Student_Id" placeholder="Student Id" required />
+                    <label>First_Name  <span>*</span></label>
+                    <input type="text" name="First_Name" placeholder="FirstName" required/>
+                    <label>Last_Name  <span>*</span></label>
+                    <input type="text" name="Last_Name" placeholder="LastName" required/>
+                    <label>Email  <span>*</span></label>
+                    <input type="email" name="Email" placeholder="email"required />
+                    <label>Password  <span>*</span></label>
+                    <input type="password" name="Password" placeholder="Password" required/>
+                    <label>Re-Password  <span>*</span></label>
+                    <input type="password" name="re-password" placeholder="Re-Password" required/>
+
+                    <div class="g-recaptcha" data-sitekey="6Ldbdg0TAAAAAI7KAf72Q6uagbWzWecTeBWmrCpJ">
+                    </div>
+                    <input type="submit" value="Join Now" class='submit' />
+                </div>
+            </form>
+        );
+    }
+}
+
+export default SignUp; 
