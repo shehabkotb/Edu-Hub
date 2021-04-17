2fd258e1c25958576b6232ed14c06d8275b5cd6f
HazemHamada <zoma_hamada@hotmail.com>
2021-04-17 17:55:02 +0200

    create the initial files and add the logger to the redux

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { combineReducers, createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import logger from 'redux-logger'
+
 import authReducer from './reducers/authReducer'
 import courseReducer from './reducers/courseReducer'
 import moduleReducer from './reducers/moduleReducer'
@@ -13,6 +13,6 @@ const reducer = combineReducers({
 })
 
 // debugging with devtools
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk,logger)))
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store
