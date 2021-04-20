1064559b4c835db2bab09fde35d3d5e99a049766
Eslam Genedy <eslamgenedy377@yahoo.com>
2021-04-20 08:06:41 +0200

    Convert Comment to functional component

--- src/components/Article/comments/index.jsx
+++ src/components/Article/comments/index.jsx
@@ -1,122 +1,112 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'antd/dist/antd.css';
 import { Comment, Avatar, Form, Button, List, Input } from 'antd';
-import moment from 'moment';
+import { useDispatch, useSelector } from 'react-redux';
+
 // import {DeleteFilled} from '@ant-design/icons'
+
 
 
 const { TextArea } = Input;
 
-const CommentList = ({ comments }) => (
-  <List
-    dataSource={comments}
-    itemLayout="horizontal"
-    renderItem={props =><> <Comment {...props} /> </>}
-  />
-);
 
 
-const Editor = ({ onChange, onSubmit, submitting, value }) => (
-  <>
-    <Form.Item>
-      <TextArea rows={4} onChange={onChange} value={value} />
-    </Form.Item>
-    <Form.Item>
-      <Button htmlType="submit" loading={submitting} onClick={onSubmit} type="primary">
-        Add Comment
-      </Button>
-    </Form.Item>
-  </>
-);
+const ArticleComments = (props) => {
 
 
-class ArticleComments extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      comments: [{
-        author: 'Han Solo',
-        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-        content: 'this is as ',
-        datetime: moment().fromNow(),
-        id:1
-      },
-      {
-        author: 'Han Solo',
-        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-        content: 'fasdfasda',
-        datetime: moment().fromNow(),
-        id:2
-      }
-      ],
-      submitting: false,
-      value: '',
-    };
+
+  const initial = [{
+    author: 'Han Solo',
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    content: 'this is as ',
+    
+    id: 1
+  },
+  {
+    author: 'Han Solo',
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    content: 'fasdfasda',
+    
+    id: 2
+  }];
+  const [value, setValue] = useState('');
+  const [submitting, setSubmitting] = useState(false);
+  const [comments, setComments] = useState(initial);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
   }
 
-
-  handleSubmit = () => {
-    if (!this.state.value) {
+  const handleSubmit = () => {
+    if (!value) {
       return;
     }
-
-    this.setState({
-      submitting: true,
-    });
-
+    setSubmitting(true);
     setTimeout(() => {
-      this.setState({
-        submitting: false,
-        value: '',
-        comments: [
-         
-          {
-            author: 'Han Solo',
-            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-            content: <p>{this.state.value}</p>,
-            datetime: moment().fromNow(),
-            id:this.state.comments.length+1
-          },
-          ...this.state.comments,
-        ],
-      });
+      setComments(comments.concat(
+        {
+          author: 'Han Solo',
+          avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+          content: <p>{value}</p>,
+          
+          id: comments.length + 1
+        }))
+
+      setSubmitting(false);
+      setValue('');
     }, 1000);
-  };
 
-  handleChange = e => {
-    this.setState({
-      value: e.target.value,
-    });
-  };
-
-  render() {
-    const { comments, submitting, value } = this.state;
-    const showEditor = this.props.showEditor;
-    return (
-      <>
-
-        { showEditor && <Comment
-          avatar={
-            <Avatar
-              src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-              alt="Han Solo"
-            />
-          }
-          content={
-            <Editor
-              onChange={this.handleChange}
-              onSubmit={this.handleSubmit}
-              submitting={submitting}
-              value={value}
-            />
-          }
-        />}
-
-        {comments.length > 0 && <CommentList comments={comments} />}
-
-      </>
-    );
   }
+
+
+  return (
+    <>
+
+      { props.showEditor && <Comment
+        avatar={
+          <Avatar
+            src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+            alt="Han Solo"
+          />
+        }
+        content={
+          <Editor
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+            submitting={submitting}
+            value={value}
+          />
+        }
+      />}
+
+      {comments.length > 0 && <CommentList comments={comments} />}
+
+    </>
+  )
 }
+
+const CommentList = ({ comments }) => {
+  return (
+     <List
+       dataSource={comments}
+       itemLayout="horizontal"
+       renderItem={props => <> <Comment {...props} /> </>}
+     />
+   )
+ };
+ 
+ 
+ const Editor = ({ onChange, onSubmit, submitting, value }) => (
+   <>
+     <Form.Item>
+       <TextArea rows={4} onChange={onChange} value={value} />
+     </Form.Item>
+     <Form.Item>
+       <Button htmlType="submit" loading={submitting} onClick={onSubmit} type="primary">
+         Add Comment
+       </Button>
+     </Form.Item>
+   </>
+ );
 
 export default ArticleComments;
