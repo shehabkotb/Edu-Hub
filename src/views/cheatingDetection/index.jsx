90e119024ba2defa01386b6ee02f66da368f2fa8
HazemHamada <zoma_hamada@hotmail.com>
2021-04-20 05:06:11 +0200

    redirect when time is out

--- src/views/cheatingDetection/index.jsx
+++ src/views/cheatingDetection/index.jsx
@@ -2,27 +2,30 @@ import CameraFeed from "./components/cameraFeed"
 import Camera from './components/camera'
 import ReactCountdownClock from 'react-countdown-clock'
 import styles from './styles.css'
+import { useHistory } from 'react-router-dom'
+
 
 const CheatingDetection = () =>{
 
     document.addEventListener('visibilitychange', function () {
         document.title = document.hidden ? "I'm away" : 'EduHub'
     })
+    const history = useHistory()
 
     return (
       <div className="container">
         <div className="camera">
           <Camera />
           <ReactCountdownClock
-            seconds={0.1 * 3600}
+            seconds={0.01 * 3600}
             color="#000"
             alpha={0.9}
             size={240}
-            //onComplete={myCallback}
+            onComplete={() => history.push(`/app`)}
           />
         </div>
       </div>
-    );
+    )
 }
 
 export default CheatingDetection
