ac4faf03ea372e0a65c8eee473dfd0c1929016ab
HazemHamada <zoma_hamada@hotmail.com>
2021-04-23 04:41:25 +0200

    finish the announcements feed

---
+++ src/views/announcements/index.jsx
@@ -0,0 +1,63 @@
+import AnnouncementCard from './components/announcementCard'
+import styles from './styles.css'
+import { useSelector, useDispatch } from 'react-redux'
+import React, { useState, useEffect } from 'react'
+import { Button, Input } from 'antd'
+import {
+  addAnnouncement,
+  getAllAnnouncements
+} from '../../reducers/announcementsReducer'
+
+const Feed = ({ announcements, user }) => {
+  return announcements.map((ann) => {
+    return (
+      <AnnouncementCard
+        key={ann._id}
+        announcement={ann}
+        user={user}
+        styles={styles}
+      />
+    )
+  })
+}
+
+const AnnouncementsFeed = ({ courseId }) => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getAllAnnouncements(courseId))
+  }, [dispatch])
+  const user = useSelector((state) => state.auth.user)
+  const announcements = useSelector((state) => state.announcements)
+  const [annText, setAnnText] = useState('')
+
+  const onPost = () => {
+    if (annText !== '') {
+      dispatch(addAnnouncement(courseId, annText))
+      setAnnText('')
+    } else console.log('cant post empty post')
+  }
+
+  const onTxtChange = (txt) => {
+    setAnnText(txt.target.value)
+  }
+
+  return (
+    <div>
+      <span>
+        <Input
+          size="large"
+          allowClear={true}
+          bordered={true}
+          value={annText}
+          placeholder="what you think"
+          onChange={onTxtChange}
+          className="txt"
+        ></Input>
+        <Button onClick={onPost}>Post</Button>
+      </span>
+      <Feed announcements={announcements} user={user} />
+    </div>
+  )
+}
+
+export default AnnouncementsFeed
