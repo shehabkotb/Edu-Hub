d62083e5e12331df35f026cbfe4fe7302970894a
HazemHamada <zoma_hamada@hotmail.com>
2021-07-18 23:29:59 +0200

    add icon and onclick listenner to the device notifications pushed

--- public/sw.js
+++ public/sw.js
@@ -3,9 +3,21 @@ self.addEventListener('push', event => {
   console.log('New notification', data)
   const options = {
     body: data.body,
-    icon: data.icon
+    icon: './alert-img.png'
   }
   event.waitUntil(
     self.registration.showNotification(data.title, options)
   );
+})
+
+self.addEventListener('notificationclick', function (e) {
+  var notification = e.notification
+  var action = e.action
+
+  if (action === 'close') {
+    notification.close()
+  } else {
+    clients.openWindow('http://localhost:3000/app')
+    notification.close()
+  }
 })
